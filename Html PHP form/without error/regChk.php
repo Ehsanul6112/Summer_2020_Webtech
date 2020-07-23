5702a177f83e8a191d7af49471fd2175f792c7b2
Ehsanul6112 <67850752+Ehsanul6112@users.noreply.github.com>
2020-07-24 00:09:28 +0600

    without error first comit

---
+++ Html PHP form/without error/regChk.php
@@ -0,0 +1,42 @@
+<?php
+
+if(isset($_POST['submit']))
+{
+	$username= $_POST['username'];
+	$usermail= $_POST['usermail'];
+	$gender= $_POST['gender'];
+	$DoB= $_POST['DoB'];
+	$Bgroup = $_POST['Bgroup'];
+	if($username == "")
+	{
+		header ('location: reg.php? error= name not found');
+	}
+	/*else if(str_word_count($username)!= 2)
+	{
+		header ('location: reg.php? error= name is short');
+	}*/
+	else if($usermail == "")
+	{
+		header ('location: reg.php? error= mail not found');
+	}
+	/*else if (!filter_var($email, FILTER_VALIDATE_EMAIL)) 
+	{
+		'location: reg.php? error= Invalid email format';
+	}*/
+	else if($gender == "")
+	{
+		header ('location: reg.php? error= gender not found');
+	}
+	else if($DoB == "")
+	{
+		header ('location: reg.php? error= DoB not found');
+	}
+	else if($Bgroup == "")
+	{
+		header ('location: reg.php? error= Blood group not found');
+	}
+	else 
+		echo 'Registraion complete';
+}
+
+?>
