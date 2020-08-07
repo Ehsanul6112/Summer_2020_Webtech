7cda7fafe68fb68b729d6ee04452c8810c43499f
Ehsanul6112 <67850752+Ehsanul6112@users.noreply.github.com>
2020-08-07 12:34:30 +0600

    register/log in using database

---
+++ Project_files/registr+login using db/logCheck.php
@@ -0,0 +1,34 @@
+<?php
+
+	if(isset($_POST['submit'])){
+
+		$uname 		= $_POST['uName'];
+		$pass 	= $_POST['pass'];
+		
+		echo $pass;
+		if(empty($uname) || empty($pass)){
+			echo "null submission";
+
+		}else{
+
+			$conn =mysqli_connect('localhost', 'root', '', 'userinfo');
+			
+			$sql = "select * from userinfo where userName = '".$uname."' and Pass = '".$pass."'";
+
+			$result = mysqli_query($conn, $sql);
+			$user 	= mysqli_fetch_assoc($result);
+
+			if(count($user) > 0 ){
+				$_SESSION['status']  = "Ok";
+				header('location: home.php');
+			}else{
+				echo "Invalid username/password";
+			}
+		}
+
+	}else{
+		header("location: login.php");
+	}
+
+?> 
+	
